b1f8cb1195c7d6bf308bca9dfc96f8724ad6d6cc
William Wells <williamwells2013@hotmail.com>
2023-09-01 16:04:18 -0300

    Working MVP of data classifier

--- data_classifier.py
+++ data_classifier.py
@@ -1,3 +1,5 @@
+# TODO: make plane name hyperlinks to google, use keyboard for selecting classification and allow going forwards and backwards, max screen
+
 import tkinter
 from PIL import Image, ImageTk
 
@@ -5,8 +7,16 @@ from dataset_extractor import DroneDatasetExtractor
 
 
 class DataClassifier:
+    current_drone_index = 0
     drones = []
     root = None
+
+    photo = None  # photo must be defined here to prevent gc from destroying it in func
+    image_label = None
+    text_label = None
+    type_sel_label = None
+    submit_button = None
+
 
     def __init__(self, drones):
         self.drones = drones
@@ -17,30 +27,47 @@ class DataClassifier:
         self.root.title('Drone Classifier')
 
         # Load and display an image
-        image = tkinter.Image.open(self.drones[0]['picture'])
-        photo = ImageTk.PhotoImage(image)
+        image = Image.open(self.drones[self.current_drone_index]['picture'])
+        self.photo = ImageTk.PhotoImage(image)
 
-        image_label = tkinter.Label(self.root, image=photo)
-        image_label.pack()
+        self.image_label = tkinter.Label(self.root, image=self.photo)
+        self.image_label.pack()
 
         # Create a text field
-        text_label = tkinter.Label(self.root, text="Enter text:")
-        text_label.pack()
+        self.text_label = tkinter.Label(self.root, text=self.create_label_text())
+        self.text_label.pack()
 
-        text_entry = tkinter.Entry(self.root)
-        text_entry.pack()
+        self.type_sel_label = tkinter.Entry(self.root)
+        self.type_sel_label.pack()
 
-        submit_button = tkinter.Button(self.root, text="Submit", command=self.classify)
-        submit_button.pack()
+        self.submit_button = tkinter.Button(self.root, text='Submit', command=self.classify)
+        self.submit_button.pack()
 
         self.root.mainloop()
 
     def classify(self):
-        for drone in self.drones:
-            drone['type'] = 'test'
+        self.drones[self.current_drone_index]['type'] = self.type_sel_label.get()
+        self.current_drone_index += 1
+
+        self.update_ui()
+
+    def update_ui(self):
+        if self.current_drone_index >= len(self.drones):
+            self.root.quit()
+        else:
+            image = Image.open(self.drones[self.current_drone_index]['picture'])
+            self.photo = ImageTk.PhotoImage(image)
+            self.image_label['image'] = self.photo
+
+            self.text_label['text'] = self.create_label_text()
+
+    def create_label_text(self):
+        return 'Enter type for ' + self.drones[self.current_drone_index]['Platform'] + ':'
 
 
 if __name__ == "__main__":
     datasetExtractor = DroneDatasetExtractor()
 
     classifier = DataClassifier(datasetExtractor.extract_details())
+
+    print(classifier.drones)
